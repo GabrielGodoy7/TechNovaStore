f28d5edcd0bafae0a0e291e5ce7c18dbfb1d2fa9
GabrielGodoy7 <142518644+GabrielGodoy7@users.noreply.github.com>
2023-08-28 23:16:12 -0300

    Conclusão do Carrinho e troca na paleta de cores

--- techNovaStore/src/app/carrinho/carrinho.page.ts
+++ techNovaStore/src/app/carrinho/carrinho.page.ts
@@ -1,28 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component } from '@angular/core';
 import { BdtempService } from '../services/bdtemp.service';
 
 @Component({
   selector: 'app-carrinho',
-  templateUrl: './carrinho.page.html',
-  styleUrls: ['./carrinho.page.scss'],
+  templateUrl: 'carrinho.page.html',
+  styleUrls: ['carrinho.page.scss'],
 })
-export class CarrinhoPage implements OnInit {
+export class CarrinhoPage {
   qtdeItensCarrinho = 0;
+  total = 0;
 
-  listaItens = [];
+  listaProdutos = [
+    {nome: "JBL - Sound Bar",
+    descricao: "Experiência sonora incomparável...",
+    preco: 1490,
+    foto:"../../assets/img/JBLBar SoundBox.png"},
 
-  constructor(public bdtemp: BdtempService) { }
+    {nome:"Alexa - Echo Dot Com Relógio",
+    descricao:"Tempo, música e muito mais...",
+    preco:1233,
+    foto: "../../assets/img/echo-dot-com-relogio-4a-geracao-smart-speaker-com-relogio-e-alexa_1_1200.png"},
+
+    {name:"Joystick Xbox Series S",
+    descricao:"Controle Ultra leve e Ergonomico",
+    preco:380,
+    foto:"../../assets/img/Xbox-Series-X-1-TB-8K-HDR-2.png"},
+  ];
+
+  constructor(private bdtempService: BdtempService) {}
 
   ngOnInit() {
   }
 
-
-  buscarItensCarrinho(){
-    this.listaItens = this.bdtemp.buscar('carrinho');
+  ionViewWillEnter() {
+    // Obtenha os itens do carrinho e o qtdeItensCarrinho do serviço ao entrar na página
+    this.listaProdutos = this.bdtempService.buscar('carrinho') || [];
+    this.qtdeItensCarrinho = this.bdtempService.buscar('qtdeItensCarrinho') || 0;
+    this.total = this.bdtempService.buscar('totalCarrinho') || 0; // Adicione esta linha para exibir o total
   }
 
-  ionViewWillEnter(){
-    this.buscarItensCarrinho();
+  addProdutoCarrinho(produto: any) {
+    this.bdtempService.addProdutoCarrinho(produto);
+    this.buscarDadosCarrinho();
   }
 
+  buscarDadosCarrinho() {
+    this.qtdeItensCarrinho = this.bdtempService.buscar('qtdeItensCarrinho');
+    this.total = this.bdtempService.buscar('totalCarrinho'); // Atualize o total após adicionar produto
+  }
+
+  removerItem(index: number) {
+    this.bdtempService.removeProdutoCarrinho(index);
+    this.listaProdutos = this.bdtempService.buscar('carrinho') || [];
+    this.qtdeItensCarrinho = this.bdtempService.buscar('qtdeItensCarrinho') || 0;
+    this.total = this.bdtempService.buscar('totalCarrinho'); // Atualize o total após remover produto
+  }
+
+  limparCarrinho() {
+    this.bdtempService.limparCarrinho();
+    this.listaProdutos = [];
+    this.qtdeItensCarrinho = 0;
+    this.total = 0; // Zere o total após limpar carrinho
+  }
+
+  concluirCompra() {
+    const carrinho = this.bdtempService.buscar('carrinho');
+
+    if (carrinho && carrinho.length > 0) {
+      // Aqui você pode realizar as ações necessárias para concluir a compra
+      // Por exemplo, salvar a compra no banco de dados, exibir uma mensagem de sucesso, etc.
+      this.bdtempService.limparCarrinho(); // Limpar o carrinho após a conclusão da compra
+      console.log('Compra concluída com sucesso.');
+    } else {
+      console.log('Não é possível concluir a compra, carrinho vazio.');
+    }
+  
+}
+
+  /*ionViewWillEnter() {
+    // Obtenha os itens do carrinho e o qtdeItensCarrinho do serviço ao entrar na página
+    this.listaProdutos = this.bdtempService.buscar('carrinho') || [];
+    this.qtdeItensCarrinho = this.bdtempService.buscar('qtdeItensCarrinho') || 0;
+  }
+
+    addProdutoCarrinho(produto: any){
+    this.bdtempService.addProdutoCarrinho(produto); // Use the injected service
+    this.buscarDadosCarrinho();
+  }
+
+  buscarDadosCarrinho(){
+    this.qtdeItensCarrinho = this.bdtempService.buscar('qtdeItensCarrinho');
+  }
+
+  removerItem(index: number) {
+    this.bdtempService.removeProdutoCarrinho(index);
+    this.listaProdutos = this.bdtempService.buscar('carrinho') || [];
+    this.qtdeItensCarrinho = this.bdtempService.buscar('qtdeItensCarrinho') || 0;
+  }
+
+  limparCarrinho() {
+    this.bdtempService.limparCarrinho();
+    this.listaProdutos = [];
+    this.qtdeItensCarrinho = 0;
+  }*/
 }
--- techNovaStore/src/app/promocoes/promocoes.page.ts
+++ techNovaStore/src/app/promocoes/promocoes.page.ts
@@ -9,30 +9,53 @@ import { BdtempService } from '../services/bdtemp.service'; // Import the servic
 export class PromocoesPage implements OnInit {
   qtdeItensCarrinho = 0;
 
-
-
   listaProdutos = [
+    {nome: "Nvidia RTX 4090",
+    descricao: "RTX 4090 da NVIDIA. Potência incomparável, ray tracing de ponta e desempenho além do imaginável.",
+    preco:4600,
+    foto:"../../assets/img/rtx_4090.png"},
+
+    {nome: "Nvidia RTX 2060",
+    descricao:"RTX 4090 da NVIDIA. Potência incomparável, ray tracing de ponta e desempenho além do imaginável.",
+    preco:3500,
+    foto:"../../assets/img/rtx_2060.png"},
+
     {nome: "JBL - Sound Bar",
-    descricao: "Experiência sonora incomparável...",
+    descricao: "Experiência sonora incomparável: a JBL Sound Bar eleva seu entretenimento a um novo patamar, oferecendo áudio de alta qualidade e imersivo em um design elegante e compacto. Sinta cada detalhe, desde os sussurros mais suaves até as explosões mais intensas, tudo com a facilidade de conectividade Bluetooth. Sua experiência audiovisual nunca mais será a mesma.",
     preco: 1490,
     foto:"../../assets/img/JBLBar SoundBox.png"},
 
-    {nome:"Alexa - Echo Dot Com Relógio",
-    descricao:"Tempo, música e muito mais...",
-    preco:1233,
-    foto: "../../assets/img/echo-dot-com-relogio-4a-geracao-smart-speaker-com-relogio-e-alexa_1_1200.png"},
+    {nome: "JBL - Home Theater Cinema 610",
+    descricao:"Transforme sua sala em um verdadeiro cinema com o JBL Home Theater Cinema 610. Áudio poderoso e envolvente que eleva sua experiência cinematográfica a outro nível. Sinta o som, mergulhe na emoção.",
+    preco: 2275,
+    foto:"../../assets/img/CINEMA_610.png"},
 
-    {name:"Joystick Xbox Series S",
-    descricao:"Controle Ultra leve e Ergonomico",
+    {nome:'Controle de PlayStation 5 DualSense',
+    descricao:'Controle Original PlayStation DualSense - Branco',
     preco:380,
-    foto:"../../assets/img/Xbox-Series-X-1-TB-8K-HDR-2.png"},
+    foto:'../../assets/img/controleps5.png'},
+
+    {nome:'Controle Xbox Series X',
+    descricao:'Controle Original Xbox Series X - Preto',
+    preco:340,
+    foto:'../../assets/img/Xbox-Series-X-1-TB-8K-HDR-2.png'},
+
+    {nome: "Samsung Galax S23 Ultra 5g",
+    descricao: "Samsung Galaxy S23 Ultra 5G - Além da Inovação, Experimente a vanguarda da tecnologia com o Galaxy S23 Ultra 5G. Câmeras avançadas, desempenho insuperável e conectividade 5G para um mundo sem limites. O futuro está em suas mãos.",
+    preco:6399,
+    foto: "../../assets/img/$_57.png"},
+
+    {nome: "Samsung Galax S22 5G",
+    descricao: "Samsung Galaxy S22 - Inovação em Suas Mãos, Desempenho excepcional, câmeras de alta qualidade e design elegante se fundem no Galaxy S22. Tecnologia de ponta para acompanhar seu estilo de vida dinâmico.",
+    preco:2698,
+    foto: "../../assets/img/S22.png"},
   ];
 
   constructor(private bdtempService: BdtempService) { } // Inject the service
 
   ngOnInit() {
   }
-  
+
   addProdutoCarrinho(produto: any){
     this.bdtempService.addProdutoCarrinho(produto); // Use the injected service
     this.buscarDadosCarrinho();
